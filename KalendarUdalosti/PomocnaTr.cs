0c2f67d4e697d909a068a686c2f45fdf25bf9346
Josef Navrátil <josef.navratil@educhem.cz>
2022-06-15 12:42:44 +0200

    ok

--- KalendarUdalosti/PomocnaTr.cs
+++ KalendarUdalosti/PomocnaTr.cs
@@ -1,55 +1,44 @@
 ﻿using Spectre.Console;
 
-namespace KalendarUdalosti{
-    internal class PomocnaTr{
+namespace KalendarUdalosti
+{
+    internal class PomocnaTr
+    {
         List<DateOnly> data = new List<DateOnly>();
 
         public void ActualMonth()
         {
+            //nacte soucasny datum
             DateOnly soucasnyDatum = DateOnly.FromDateTime(DateTime.Now);
 
+            //oznací cervene v kalendari
             var calendar = new Calendar(soucasnyDatum.Year, soucasnyDatum.Month);
             calendar.AddCalendarEvent(soucasnyDatum.Year, soucasnyDatum.Month, soucasnyDatum.Day);
             AnsiConsole.Write(calendar.HighlightStyle(Style.Parse("red bold")));
-            
+
         }
 
         public void AllEvents(DateTime dateTime)
         {
-            foreach (DateOnly date in this.data)
-            {
-                dateTime = date.ToDateTime(TimeOnly.Parse("00:00 AM"));
-            }
-            while (true)
-            {
-                if (dateTime != DateTime.MinValue)
-                {
-                    var vyber = AnsiConsole.Prompt(
-                    new SelectionPrompt<DateTime>()
-                    .Title("Events:")
-                    .PageSize(10)
-                    .AddChoices(new[] { dateTime }));
-                    var calendar = new Calendar(vyber);
-                    calendar.AddCalendarEvent(vyber);
-                    AnsiConsole.Write(calendar.HighlightStyle(Style.Parse("yellow bold")));
-                    Console.ReadKey();
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("error");
-                    Console.ResetColor();
-                    break;
-                }
-                
-            }
+            //vyber z DateonlyListu - potreba prevest na DateTime, jinak nejde vybrat
+            var vyber = AnsiConsole.Prompt(new SelectionPrompt<DateTime>()
+                .Title("Events:")
+                .PageSize(10)
+                .AddChoices(this.data.Select(x => x.ToDateTime(TimeOnly.Parse("00:00 AM"))))
+            );
+
+            Calendar calendar = new Calendar(vyber);
+            calendar.AddCalendarEvent(vyber);
+            //barvicky
+            AnsiConsole.Write(calendar.HighlightStyle(Style.Parse("yellow bold")));
+            Console.ReadKey();
         }
 
         public void NewEvent()
         {
             while (true)
             {
+                //nacitani roku, mesice, dne
                 Console.Write("year: ");
                 int year = Convert.ToInt32(Console.ReadLine());
                 if (year >= 1950 && year <= 2022) { }
@@ -75,6 +64,7 @@ namespace KalendarUdalosti{
                     break;
                 }
 
+                //vytvori novy dateonly a prida to do Listu 'data'
                 DateOnly date = new DateOnly(year, month, day);
                 data.Add(date);
                 break;
@@ -83,6 +73,7 @@ namespace KalendarUdalosti{
 
         public void Helper()
         {
+            //vypise cervene error a bude trvat 3s
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("error");
             Console.ResetColor();
